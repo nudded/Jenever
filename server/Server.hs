8d0924ad3cfca4084b21587c3aeaec3285b676a0
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-02-02 22:53:27 +0100

    Removed fork bomb.

--- server/Server.hs
+++ server/Server.hs
@@ -11,21 +11,24 @@ import Control.DeepSeq
 
 restore :: IO Jenever
 restore = do exists <- doesFileExist "jenever.previous"
-             if exists then decodeFile "jenever.previous"
+             if exists then do copyFile "jenever.current" "jenever.previous"
+                               decodeFile "jenever.previous"
                        else return newJenever
 
 store :: Jenever -> IO ()
 store jenever = encodeFile "jenever.current" jenever
 
 main :: IO ()
-main = do copyFile "jenever.current" "jenever.previous"
-          socket <- listenOn (PortNumber 2625)
+main = do socket <- listenOn (PortNumber 2625)
           jenever <- restore
-          count <- newMVar jenever
-          forever (listen count socket)
+          mvar <- newMVar jenever
+          forever (listen mvar socket)
   where
     listen mvar socket = do
         (handle, _, _) <- accept socket
+        forkIO $ respond mvar handle
+
+    respond mvar handle = do
         message <- hGetLine handle
         let parsed = parseCommand message
         case parsed of
