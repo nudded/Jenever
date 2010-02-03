3cbf80ce54b096870237aa8499f638fd5b460806
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-02-03 13:21:08 +0100

    Daemonized server.

--- server/Server.hs
+++ server/Server.hs
@@ -7,6 +7,10 @@ import System.IO
 import Network
 import System.Directory
 import System.FilePath ((</>))
+import System.Posix.Daemonize
+import System.Posix
+import System.Posix
+import Data.Maybe (fromJust)
 
 import Control.DeepSeq
 import Data.Binary
@@ -14,38 +18,34 @@ import Data.Binary
 import Jenever
 
 -- | Application directory to store dumps.
-appDirectory :: IO FilePath
-appDirectory = getAppUserDataDirectory "jenever"
+dumpDirectory :: FilePath
+dumpDirectory = "/var/lib/jenever"
 
 -- | Current dump file.
-currentDump :: IO FilePath
-currentDump = (</> "current") <$> appDirectory
+currentDump :: FilePath
+currentDump = dumpDirectory </> "current"
 
 -- | Previous dump file.
-previousDump :: IO FilePath
-previousDump = (</> "previous") <$> appDirectory
+previousDump :: FilePath
+previousDump = dumpDirectory </> "previous"
 
 -- | Restore the Jenever instance from the last dump, or create a new empty
 --   instance if there is no dump.
 restore :: IO Jenever
-restore = do currentDump' <- currentDump
-             previousDump' <- previousDump
-             exists <- doesFileExist currentDump'
-             if exists then do copyFile currentDump' previousDump'
-                               decodeFile previousDump'
+restore = do exists <- doesFileExist currentDump
+             if exists then do copyFile currentDump previousDump
+                               decodeFile previousDump
                        else return newJenever
 
 store :: Jenever -> IO ()
-store jenever = do currentDump' <- currentDump
-                   encodeFile currentDump' jenever
+store jenever = encodeFile currentDump jenever
 
-main :: IO ()
-main = do -- Ensure the directories exist.
-          createDirectoryIfMissing True =<< appDirectory
-          socket <- listenOn (PortNumber 2625)
-          jenever <- restore
-          mvar <- newMVar jenever
-          forever (listen mvar socket)
+runJenever :: Logger -> IO ()
+runJenever _ = do -- Ensure the directories exist.
+    socket <- listenOn (PortNumber 2625)
+    jenever <- restore
+    mvar <- newMVar jenever
+    forever (listen mvar socket)
   where
     listen mvar socket = do
         (handle, _, _) <- accept socket
@@ -62,3 +62,10 @@ main = do -- Ensure the directories exist.
                                  hPutStrLn handle r
                                  putMVar mvar j
         hClose handle
+
+main :: IO ()
+main = do createDirectoryIfMissing True dumpDirectory
+          user <- userID <$> getUserEntryForName "daemon"
+          group <- groupID <$> getGroupEntryForName "daemon"
+          setOwnerAndGroup dumpDirectory user group
+          serviced runJenever
