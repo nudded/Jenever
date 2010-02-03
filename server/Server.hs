f6ca310e27c37386f3133a33ed580ed95b5b7800
Jasper Van der Jeugt <jaspervdj@gmail.com>
2010-02-03 12:28:51 +0100

    Now stores dumps in ~/.jenever.

--- server/Server.hs
+++ server/Server.hs
@@ -2,24 +2,47 @@ module Main where
 
 import Control.Monad.State
 import Control.Concurrent
+import Control.Applicative ((<$>))
 import System.IO
 import Network
-import Jenever
 import System.Directory
-import Data.Binary
-import Control.DeepSeq
+import System.FilePath ((</>))
 
+import Control.DeepSeq
+import Data.Binary
+
+import Jenever
+
+-- | Application directory to store dumps.
+appDirectory :: IO FilePath
+appDirectory = getAppUserDataDirectory "jenever"
+
+-- | Current dump file.
+currentDump :: IO FilePath
+currentDump = (</> "current") <$> appDirectory
+
+-- | Previous dump file.
+previousDump :: IO FilePath
+previousDump = (</> "previous") <$> appDirectory
+
+-- | Restore the Jenever instance from the last dump, or create a new empty
+--   instance if there is no dump.
 restore :: IO Jenever
-restore = do exists <- doesFileExist "jenever.previous"
-             if exists then do copyFile "jenever.current" "jenever.previous"
-                               decodeFile "jenever.previous"
+restore = do currentDump' <- currentDump
+             previousDump' <- previousDump
+             exists <- doesFileExist currentDump'
+             if exists then do copyFile currentDump' previousDump'
+                               decodeFile previousDump'
                        else return newJenever
 
 store :: Jenever -> IO ()
-store jenever = encodeFile "jenever.current" jenever
+store jenever = do currentDump' <- currentDump
+                   encodeFile currentDump' jenever
 
 main :: IO ()
-main = do socket <- listenOn (PortNumber 2625)
+main = do -- Ensure the directories exist.
+          createDirectoryIfMissing True =<< appDirectory
+          socket <- listenOn (PortNumber 2625)
           jenever <- restore
           mvar <- newMVar jenever
           forever (listen mvar socket)
